9107dcca59d0a88d48e2d9508d5150054a511840
Matthew Younger <matthewyounger33@gmail.com>
2021-12-21 14:03:18 -0600

    renamed to non-blacklisted directory

--- library/newhost.py
+++ library/newhost.py
@@ -1,24 +1,33 @@
 from shutil import copyfile
-from wgkey import genkeys
+import library.wgkey
+# from wgkey import genkeys
 
 # global static: ../hosts location:
 hosts_dir = '/etc/wireguard/hosts/'
 main_cfg_dir = '/etc/wireguard/'
 
-def newhost(hostname='wg0', ip_cidr='10.0.0.0/24', listenport=51820):
+def newhost(hostname, ip_cidr, listenport):
     '''
     Responsible for creating a new "host" interface,
     writing the interface to /etc/wireguard/.conf 
     and backing up a copy to /etc/wireguard/hosts/.conf.bak
     '''
 
+    # replace default values if passed a None type:
+    if hostname == None:
+        hostname = 'wg0'
+    if ip_cidr == None:
+        ip_cidr = '10.0.0.1/24'
+    if listenport == None:
+        listenport = 51820
+        
     # PLACEHOLDER VALUES, REPLACE LATER!!
     PostUp = '#/etc/wireguard/PostUp.sh'
     PostDown = '#/etc/wireguard/PostDown.sh'
     
     # call lib.wgkey.genkey to fetch values for function
     #   and save those values for future use:
-    KeyPair = genkeys(hostname, hosts_dir)
+    KeyPair = library.wgkey.genkeys(hostname, hosts_dir)
 
     with open(f'/etc/wireguard/{hostname}.conf', 'w') as f:     # write /etc/wireguard/.conf
                 f.writelines([
