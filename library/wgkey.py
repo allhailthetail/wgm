54d0596daab5066341136d1687b8970ae8c60046
Matthew Younger <matthewyounger33@gmail.com>
2021-12-21 21:12:46 -0600

    added new function get_host_public

--- library/wgkey.py
+++ library/wgkey.py
@@ -1,6 +1,6 @@
-import subprocess
+import subprocess, os
 
-def genkeys(name, path):
+def genkeys():
     
     """
     Generate a WireGuard private & public key
@@ -8,21 +8,20 @@ def genkeys(name, path):
     Returns (private_key, public_key), both strings
     """
     
-    # define new variable:
     KeyPair = {'privkey':'', 'pubkey':''}
 
     # fill values:
     KeyPair['privkey'] = subprocess.check_output("wg genkey", shell=True).decode("utf-8").strip()
     privkey = KeyPair['privkey']
     KeyPair['pubkey'] = subprocess.check_output(f"echo '{privkey}' | wg pubkey", shell=True).decode("utf-8").strip()
-    
-    with open(f'{path+name}.public', 'w') as f:     # write public key
-                f.write(f"{KeyPair['pubkey']}\n")
-                f.close()
 
-    with open(f'{path+name}.private', 'w') as f:    # write private key
-                f.write(f"{KeyPair['privkey']}\n")
-                f.close()
-    
     # return keypair back to program for further processing:
     return(KeyPair)
+
+def get_host_public(hostname):
+    """
+    processes the host.private file and returns the corresponding public key 
+    """
+    os.path.isdir('/etc/wireguard/{hostname}.d/')
+    os.path.isfile('/etc/wireguard/{hostname.d/{hostname}.private')
+    return subprocess.check_output(f'wg pubkey < /etc/wireguard/{hostname}.d/{hostname}.private', shell=True).decode('utf-8').strip()
