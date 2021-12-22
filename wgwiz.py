8fce9b1e60e59742b7db97e38655703dceddef65
Matthew Younger <matthewyounger33@gmail.com>
2021-12-21 23:23:50 -0600

    cleaning up arguments, still issue in 'allowed ip'

--- wgwiz.py
+++ wgwiz.py
@@ -1,5 +1,5 @@
-import os, sys, subprocess
-import help
+import os, sys, subprocess, argparse
+import library.newhost, library.newclient
 
 def is_root():
     if os.geteuid() == 0:
@@ -7,29 +7,78 @@ def is_root():
     else:
         return False
 
-def get_args():
-        return sys.argv[1:]
-
-# def newhost():
-
 def main():
-    # Get arguments:
-    args = get_args()
 
     # Check if root before continuing:
     if is_root():
-        print(len(get_args()))
+        parser = argparse.ArgumentParser(description='wgwiz utility:')
+
+        parser.add_argument('-N', '--newhost',
+            dest='HOSTNAME',
+            help='Create a new host interface, <hostname>.conf')
+
+        parser.add_argument('-n', '--newclient', 
+            dest='CLIENTNAME',
+            help='Create a new host interface, <clientname>.conf')
+
+        parser.add_argument('-p', '--port',
+            dest='PORT',
+            help='Specify port for host or client')
+
+        parser.add_argument('-I', '--ip-host',
+            dest='IP_HOST',
+            help='Specify host IPv4 Address x.x.x.x/x in CIDR')
+        
+        parser.add_argument('-i', '--ip',
+            dest='IP',
+            help='Specify target IPv4 Address x.x.x.x/x in CIDR')
+        
+        parser.add_argument('-b', '--bind-to',
+            dest='BIND_TO',
+            help='tell client which host to bind to')
+
+        parser.add_argument('-e','--endpoint',
+            dest='ENDPOINT',
+            help='tell client public host address')
+        
+        parser.add_argument('--allowed',
+            dest='ALLOWED',
+            help='allowed addresses')
+        
+        parser.add_argument('--dns',
+            dest='DNS',
+            help='specify DNS Nameservers')
+
+        args = parser.parse_args()
+
+        # cannot create host and client in one line, exit
+        if args.HOSTNAME and args.CLIENTNAME:
+            sys.exit('hosts and clients must be created separately!!')
+
+        # create new 'host' WireGuard interface
+        if args.HOSTNAME:                            
+            library.newhost.newhost(
+                hostname=args.HOSTNAME, 
+                ip_cidr=args.IP, 
+                listenport=args.PORT)   
+        
+        # create new 'client' WireGuard interface
+        if args.CLIENTNAME:
+            library.newclient.newclient(
+                hostname = args.BIND_TO,
+                clientname=args.CLIENTNAME,
+                client_ip=args.IP,
+                host_endpoint = args.ENDPOINT,
+                #Allowed has a problem, allows match on client and server!
+                #This should be different between the two!
+                allowed_ip=args.ALLOWED,
+                dns = args.DNS,
+                host_endpoint_port = args.PORT
+            )
     
     # If not root,
-    elif not is_root():
-        # -h or --help opens man page without privileges:
-        if len(args) == 1 and (args[0] == '-h' or args[0] == '--help'):
-            help.manpage()
-        elif len(args) == 1 and (args[0] != '-h' or args[0] != '--help'):
-            help.warn_no_root()
-    # Panic Condition:
     else: 
-        help.panic_exit()
+        sys.exit('must run as root!!')
 
 
 
