26135e60a5b7792daec7793484eeb28697cef447
Matthew Younger <matthewyounger33@gmail.com>
2021-12-22 16:21:33 -0600

    static loading

--- src/wgm/__init__.py
+++ src/wgm/__init__.py
@@ -1,5 +1,5 @@
 # static initialization 
-import main         # main required to run program
+import wgm.wgm as wgm         # main required to run program
 import wgkey        # required for both newhost and newclient
 import newhost      # newclient is dependent on host being created first
 import newclient    # newclient ready to initialize
