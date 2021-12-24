74f4fd96c0f585a5fb3e5bb6c1e07ccea00cf5b2
Matthew Younger <matthewyounger33@gmail.com>
2021-12-23 23:12:53 -0600

    add comments, remove test code

--- src/wgm/networkinfo.py
+++ src/wgm/networkinfo.py
@@ -2,16 +2,14 @@ import ipaddress
 
 # usable client addresses is total - 2
 def max_usable_hosts(addr):
+    """
+    compute total number of client connections
+    that can be made with a given network
+    """
     return ipaddress.ip_network(addr).num_addresses - 2
 
 def get_host_list(addr):
+    """
+    give a range of available client and host addresses,
+    """
     return ipaddress.ip_network(addr).hosts()
-
-
-
-
-print(max_usable_hosts('10.0.0.0/32'))
-
-print(list(get_host_list('10.0.0.0/32')))
-
-# print(ipaddress.ip_network('10.0.0.0/24').num_addresses)
